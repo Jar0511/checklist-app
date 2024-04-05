371b570bb7f7722f1d0a50c539cd4d642fabfb0a
ahreum.J <jar@ibstech.kr>
2024-04-05 11:35:23 +0900

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "grapefruit": {
+          "50": 'rgb(253 237 236 / <alpha-value>)',
+          "100": 'rgb(252 220 217 / <alpha-value>)',
+          "200": 'rgb(249 185 180 / <alpha-value>)',
+          "300": 'rgb(245 149 142 / <alpha-value>)',
+          "400": 'rgb(242 114 105 / <alpha-value>)',
+          "500": 'rgb(239 79 67 / <alpha-value>)',
+          "600": 'rgb(215 71 60 / <alpha-value>)',
+          "700": 'rgb(191 63 54 / <alpha-value>)',
+          "800": 'rgb(167 55 47 / <alpha-value>)',
+          "900": 'rgb(143 47 40 / <alpha-value>)',
+          "950": 'rgb(72 24 20 / <alpha-value>)',
+        }
+      }
+    },
     fontFamily: {
       Pretendard: ["Pretendard", 'sans-serif']
     }
