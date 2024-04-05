ab4e77ef047440a496f0c08234752538dcd3fd08
ahreum.J <jar@ibstech.kr>
2024-04-05 11:58:52 +0900

    move main(router config) to app

---
+++ src/app/App.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+import { supabase } from '@/shared/api'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      console.log(event, session);
+    })
+
+    return () => data.subscription.unsubscribe();
+  }, []);
+
+  return (
+    <section className='font-Pretendard'>
+      <Outlet />
+    </section>
+  )
+}
+
+export default App
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,41 +1,19 @@
 // FSD의 최상층 레이어인 app에는 앱의 진입점(entry point)과 앱의 라우트 구성 등 앱 전반에 대한 구성이 포함된다.
-import { useEffect, useState } from 'react'
-import viteLogo from '/vite.svg'
-import { supabase } from '@/shared/api'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '@/app/App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+]);
 
-  useEffect(() => {
-    const { data } = supabase.auth.onAuthStateChange((event, session) => {
-      console.log(event, session);
-    })
-
-    return () => data.subscription.unsubscribe();
-  }, []);
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>{import.meta.env.MODE}</p>
-    </>
-  )
-}
-
-export default App
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
