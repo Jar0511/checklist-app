a7c668cc060d6cd7c9bdbceccb5eb065e2fcba89
ahreum.J <jar@ibstech.kr>
2024-04-09 11:28:53 +0900

    다크모드 대응

--- src/shared/ui/StyledBlocks.tsx
+++ src/shared/ui/StyledBlocks.tsx
@@ -3,9 +3,9 @@ import { IconType } from "react-icons";
 
 export const Callout = ({icon: Icon, className, children}: {icon?: string | IconType ,className?: string; children?: ReactNode}) => {
   return (
-    <div className={`${className ?? ''} ${className?.includes("bg-") ? '' : 'bg-neutral-200'} rounded-md py-3 px-2 flex items-start gap-x-2`}>
+    <div className={`${className ?? ''} ${className?.includes("bg-") ? '' : 'bg-neutral-200'} text-stone-900 rounded-md py-3 px-2 flex items-start gap-x-2`}>
       <div>{Icon ? (typeof Icon == "string" ? Icon : <Icon />) : '💡'}</div>
-      <div className="flex-1">{children}</div>
+      <div className="flex-1 break-all whitespace-pre-line">{children}</div>
     </div>
   )
 }
