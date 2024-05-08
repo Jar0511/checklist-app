c20d9fd00afeaa627d5bc1103f59b8f64aaeb9f0
ahreum.J <jar@ibstech.kr>
2024-05-08 17:22:28 +0900

    프로필 영역 다크모드 대응

--- src/shared/ui/ProfileIcon.tsx
+++ src/shared/ui/ProfileIcon.tsx
@@ -5,14 +5,16 @@ export const ProfileIcon = ({url, bg, size = "sm"}: {url?: string | null; size?:
     <div
       className={`${
           size == "lg" ? "w-[56px] h-[56px]" : "w-[32px] h-[32px]"
-        } rounded-full overflow-hidden flex items-center justify-center`}
+        } rounded-full overflow-hidden flex items-end justify-center ${
+          bg ? '' :"border border-solid border-neutral-400 dark:border-neutral-200"}
+        `}
       style={{
         backgroundColor: bg ?? 'transparent'
       }}
     >
       {url ?
         <img src={url} /> :
-        <FaUser />
+        <FaUser className="size-[80%] fill-neutral-400 dark:fill-neutral-200" />
       }
     </div>
   )
