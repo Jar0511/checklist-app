9c1651ae0b18816a8878ad603aaebecdd0f02c80
ahreum.J <jar@ibstech.kr>
2024-05-17 13:34:39 +0900

    createPortal() 모달 컴포넌트 내에서 처리

--- src/shared/ui/Overlays.tsx
+++ src/shared/ui/Overlays.tsx
@@ -1,10 +1,23 @@
+import { useAtomValue } from "jotai";
+import { createPortal } from "react-dom";
+import { modalShowAtom } from "../model";
+
 export const Modal = () => {
+  const show = useAtomValue(modalShowAtom);
+  const portal = document.getElementById("portal") as Element;
 
   return (
-    <div className="fixed top-0 left-0 z-50 w-screen h-screen bg-black/50">
-      <div className="absolute -translate-x-1/2 -translate-y-1/2 bg-white top-1/2 left-1/2 font-Pretendard text-neutral-900 dark:bg-stone-900 dark:text-stone-50 w-[288px] sm:w-[300px] md:w-[420px] rounded-xl h-auto max-h-[80vh]">
-        ???
-      </div>
-    </div>
+    <>
+      {createPortal(
+        show ?
+        <div className="fixed top-0 left-0 z-50 w-screen h-screen bg-black/50">
+          <div className="absolute -translate-x-1/2 -translate-y-1/2 bg-white top-1/2 left-1/2 font-Pretendard text-neutral-900 dark:bg-stone-900 dark:text-stone-50 w-[288px] sm:w-[300px] md:w-[420px] rounded-xl h-auto max-h-[80vh]">
+            ???
+          </div>
+        </div>
+        : <></>,
+        portal
+      )}
+    </>
   )
 }
