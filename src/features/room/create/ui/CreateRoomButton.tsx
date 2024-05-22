0ba3420932075eb36af19315ce5961a8c82f0c4b
ahreum.J <jar@ibstech.kr>
2024-05-22 14:31:56 +0900

    방 설명 항목 추가

--- src/features/room/create/ui/CreateRoomButton.tsx
+++ src/features/room/create/ui/CreateRoomButton.tsx
@@ -1,7 +1,7 @@
 import { userAtom } from "@/entities/auth";
 import { modalShowAtom, required, trimmed } from "@/shared/model";
 import { Tables } from "@/shared/model/supabase";
-import { CustomButton, CustomInput, Modal } from "@/shared/ui";
+import { CustomButton, CustomInput, CustomLabel, Modal } from "@/shared/ui";
 import { useAtomValue, useSetAtom } from "jotai";
 import { useForm } from "react-hook-form";
 import { MdGroups } from "react-icons/md";
@@ -16,20 +16,29 @@ export const CreateRoomButton = () => {
   const [loading, setLoading] = useState(false);
   const {
     register,
-    handleSubmit
+    handleSubmit,
+    formState: {isValid, errors}
   } = useForm<Tables<"room">>();
   const nameRegister = register("room_nm", {required, setValueAs: trimmed});
+  const descRegister = register("room_desc", {setValueAs: trimmed});
   return (
     <>
       <Modal
         outsideClose
         actionButtons={
-          <CustomButton type="submit" form="newRoomForm">확인</CustomButton>
+          <CustomButton
+            type="submit"
+            form="newRoomForm"
+            disabled={!isValid}
+          >
+            확인
+          </CustomButton>
         }
         loading={loading}
+        align="left"
       >
         <h3>신규 방 생성</h3>
-        <p>방 이름을 입력하세요</p>
+        <p>방 이름은 필수 정보입니다</p>
         <form
           id={"newRoomForm"}
           onSubmit={handleSubmit(async ({room_nm}) => {
@@ -44,14 +53,20 @@ export const CreateRoomButton = () => {
               setLoading(false);
             }
           })}
+          className="flex w-full gap-2"
         >
-          <CustomInput className="border-neutral-500" {...nameRegister} />
+          <CustomLabel direction="portrait" required>
+            <span>방 이름</span>
+            <CustomInput className="border-neutral-500" {...nameRegister} err={errors?.room_nm?.message} />
+          </CustomLabel>
+          <CustomLabel direction="portrait">
+            <span>설명</span>
+            <CustomInput className="border-neutral-500" {...descRegister} />
+          </CustomLabel>
         </form>
       </Modal>
       <CustomButton
         modal
-        btnstyle="inline"
-        className="hover:bg-stone-500/15 dark:hover:bg-stone-300/25"
         onClick={() => setShow(true)}
         title="방 생성"
       >
