f0f4f8b54032d7253d4ac2e553740d04668a3805
ahreum.J <jar@ibstech.kr>
2024-05-22 16:11:05 +0900

    방 카드 드롭다운 메뉴 추가

--- src/widgets/ui/Card.tsx
+++ src/widgets/ui/Card.tsx
@@ -1,10 +1,12 @@
 import { userAtom } from "@/entities/auth";
 import { getRoomMembers } from "@/features/room/setting";
+import { useDismissClick } from "@/shared/lib";
 import { Tables } from "@/shared/model/supabase";
-import { SkeletonWrapper } from "@/shared/ui";
+import { CustomButton, DropDownWrapper, SkeletonWrapper } from "@/shared/ui";
 import { useAtomValue } from "jotai";
 import { HTMLAttributes, Suspense, useEffect, useState } from "react";
 import { FaHouseUser, FaUserGroup } from "react-icons/fa6";
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
 import { PiCalendarStarFill } from "react-icons/pi";
 
 const AdditionalInfo = ({children, ...rest}: HTMLAttributes<HTMLSpanElement>) => (
@@ -23,6 +25,7 @@ export const RoomCard = ({
 }: Partial<Tables<"room">> & {room_owner_nm?: string | null}) => {
   const session = useAtomValue(userAtom);
   const [members, setMembers] = useState<null | number>(null);
+  const [openMenu, setOpenMenu] = useState(false);
 
   useEffect(() => {
     if(_id != undefined) {
@@ -32,11 +35,14 @@ export const RoomCard = ({
       })()
     }
   }, [_id]);
+
+  useDismissClick(`.card_menu_container`, () => setOpenMenu(false));
+
   return (
-    <div className="border border-solid rounded shadow-sm border-neutral-200 dark:border-neutral-400 px-[20px] py-[16px] leading-tight flex sm:flex-row flex-col sm:items-start gap-[16px] dark:bg-stone-800 cursor-pointer">
-      <div className="flex items-start sm:gap-[16px] gap-[8px] max-w-[40%]">
-        <span title={current_banner_id ? "프로필 뽑기 진행 중" : ""} className="w-[1.25rem] h-[1.25rem] inline-flex items-center justify-center">
-          {!!current_banner_id && <PiCalendarStarFill className="size-[1.25rem] fill-grapefruit-500 dark:fill-grapefruit-700" />}
+    <div onClick={() => console.log("click div")} className="border border-solid rounded shadow-sm border-neutral-200 dark:border-neutral-400 px-[20px] py-[16px] leading-tight flex sm:flex-row flex-col sm:items-center gap-[16px] dark:bg-stone-800 cursor-pointer">
+      <div className="flex items-center sm:gap-[16px] gap-[8px] max-w-[40%]">
+        <span title={current_banner_id ? "프로필 뽑기 진행 중" : ""} className="w-[1.25rem] h-[1.25rem] inline-flex self-start items-center justify-center">
+          <PiCalendarStarFill className={`size-[1.25rem] ${current_banner_id ? "fill-grapefruit-500 dark:fill-grapefruit-700" : "fill-neutral-400 dark:fill-neutral-400"} `} />
         </span>
         <h2 className="font-bold dark:text-white">{room_nm}</h2>
       </div>
@@ -58,6 +64,30 @@ export const RoomCard = ({
             {members}
           </AdditionalInfo>
         </Suspense>
+        <div className="relative ">
+          <CustomButton
+            aria-label="toggle menu"
+            btnstyle="inline"
+            size="sm"
+            className="hover:bg-neutral-200 dark:hover:bg-neutral-600"
+            onClick={(e) => {
+              e.stopPropagation();
+              setOpenMenu(prev => !prev);
+            }}
+          >
+            <HiOutlineDotsHorizontal className="text-neutral-500 dark:text-neutral-300" />
+          </CustomButton>
+          {openMenu &&
+            <DropDownWrapper
+              width={"100px"}
+              top={"90%"}
+              right={0}
+              className={`*:px-2 card_menu_container`}
+            >
+              <p>메뉴</p>
+            </DropDownWrapper>
+          }
+        </div>
       </div>
     </div>
   )
