29f02756d7f0b7c8c3adeb833fed2f66e502b47c
ahreum.J <jar@ibstech.kr>
2024-05-28 13:40:02 +0900

    체크리스트 쿼리 추가

--- src/features/inside-room/checklist/api/index.ts
+++ src/features/inside-room/checklist/api/index.ts
@@ -1,4 +1,5 @@
 import { supabase } from "@/shared/api"
+import { Tables } from "@/shared/model/supabase";
 
 /** 활성화된 체크리스트 조회 */
 export const getChecklist = async (room_id: number) => {
@@ -28,4 +29,41 @@ export const getCheckWaitlist = async (room_id: number) => {
     }
 
     return data;
+}
+
+/** 신규 체크리스트 추가 */
+export const postNewChecklist = async (body: Partial<Tables<'checklist'>>) => {
+  const { error } = await supabase
+    .from('checklist')
+    .insert([
+      {...body, checked: false}
+    ])
+
+    if(error) {
+      throw new Error(`체크리스트 추가 중 오류: ${error}`)
+    }
+}
+
+/** 체크리스트 토글 */
+export const postToggleChecklist = async (body: Pick<Tables<'checklist'>, "checked"|"id">) => {
+  const { error } = await supabase
+    .from('checklist')
+    .update(body)
+    .eq('id', body.id)
+
+    if(error) {
+      throw new Error(`체크리스트 상태 변경 중 오류: ${error}`)
+    }
+
+    if(body.checked) {
+      const { error } = await supabase
+        .rpc('increment_count', {
+          _id: body.id,
+          x: 1
+        })
+
+        if(error) {
+          throw new Error(`체크리스트 사용 횟수 변경 중 오류: ${error}`)
+        }
+    }
 }
