2efcbae88450b3d564451d1482783912e9f4ece5
ahreum.J <jar@ibstech.kr>
2024-06-13 17:57:03 +0900

    .

--- src/features/profile/upload/model/action.ts
+++ src/features/profile/upload/model/action.ts
@@ -3,15 +3,15 @@ import { FolderAtom, FolderErrAtom, FolderLoading } from "./store";
 import { getFolderList } from "../api";
 import type { ErrorResponse } from "react-router-dom";
 
-export const fetchFolder = atom(null, async (get, set) => {
+export const fetchFolder = atom(null, async (get, set, path?: string, firstFetch?: boolean) => {
   const {page, folder, more} = get(FolderAtom);
   if(!more) return;
   set(FolderLoading, true);
   try {
-    const list = await getFolderList(page + 1, 50);
+    const list = await getFolderList(page + 1, 50, path);
 
     set(FolderAtom, {
-      folder: [...folder, ...list.map((folder) => folder.name)],
+      folder: [...(firstFetch ? [] : folder), ...list.map((folder) => folder.name)],
       page: page + 1,
       more: list.length >= 50
     });
