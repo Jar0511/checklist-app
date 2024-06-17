5c05c14f58dbf8b45ae82a83583ddf9698e69585
ahreum.J <jar@ibstech.kr>
2024-06-17 17:10:54 +0900

    호출하는 컴포넌트가 상위의 usedismiss에 의존하는 경우에 대응하기 위해 가드 처리

--- src/shared/ui/Overlays.tsx
+++ src/shared/ui/Overlays.tsx
@@ -20,10 +20,11 @@ const ModalContainer = ({
   align
 }: ModalType) => {
   const setShow = useSetAtom(modalShowAtom);
-  useDismissClick(["#modal-container", ".modal-btn"], () => (outsideClose && !noDismiss && !loading) ? setShow(null) : null);
+  useDismissClick([".modal-container", ".modal-btn"], () => (outsideClose && !noDismiss && !loading) ? setShow(null) : null);
+
   return (
     <div className="fixed top-0 left-0 z-50 w-screen h-screen bg-black/50 backdrop-blur-sm">
-      <div id="modal-container" className="absolute -translate-x-1/2 -translate-y-1/2 bg-white top-1/2 left-1/2 font-Pretendard text-neutral-900 dark:text-stone-50 dark:bg-neutral-800 dark:border dark:border-neutral-400 w-[288px] sm:w-[300px] md:w-[420px] rounded-xl h-auto max-h-[80vh] flex flex-col gap-8 items-center">
+      <div onClick={(e) => e.stopPropagation()} className="modal-container absolute -translate-x-1/2 -translate-y-1/2 bg-white top-1/2 left-1/2 font-Pretendard text-neutral-900 dark:text-stone-50 dark:bg-neutral-800 dark:border dark:border-neutral-400 w-[288px] sm:w-[300px] md:w-[420px] rounded-xl h-auto max-h-[80vh] flex flex-col gap-8 items-center">
         {loading && <LoadingFallback className="absolute" blur />}
         <div className={`flex flex-col ${bgColor ?? ''} w-full`}>
           {!noDismiss &&
