102c0937c5cc8cfbbe9f48faf99ae0dc1a82ab93
ahreum.J <jar@ibstech.kr>
2024-07-02 16:02:44 +0900

    타입에러 수정(쿼리 함수 완성)

--- src/features/room/invite/api/index.ts
+++ src/features/room/invite/api/index.ts
@@ -2,9 +2,16 @@ import { supabase } from "@/shared/api";
 import type { Tables } from "@/shared/model/supabase";
 
 export const postNewMember = async (
-	id: Tables<"user">["_id"]
+	user_id: Tables<"user">["_id"],
+	room_id: Tables<"room">["_id"]
 ) => {
-	const { data, error } = await supabase
+	const { error } = await supabase
 		.from("room_user")
-		.insert([]);
+		.insert([
+			{user_id, room_id}
+		]);
+
+		if(error){
+			throw new Error(`신규 멤버 추가 중 에러: ${error}`);
+		}
 };
