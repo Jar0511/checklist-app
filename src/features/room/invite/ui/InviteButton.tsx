699984bfa8f86f268ad939c04f0f4dc3c1fbd968
ahreum.J <jar@ibstech.kr>
2024-07-02 15:57:09 +0900

    .

--- src/features/room/invite/ui/InviteButton.tsx
+++ src/features/room/invite/ui/InviteButton.tsx
@@ -2,9 +2,8 @@ import {
 	BasicButton,
 	FilledButton,
 	Modal,
-	TabPanel,
 } from "@/shared/ui";
-import { useState, type ButtonHTMLAttributes } from "react";
+import { type ButtonHTMLAttributes } from "react";
 import {
 	MdAddLink,
 	MdOutlineContentCopy,
@@ -13,26 +12,13 @@ import {
 export const InviteButton = ({
 	className,
 }: ButtonHTMLAttributes<HTMLButtonElement>) => {
-	const GenerateType = [
-		{ children: "등록 시각 제한", value: "time" },
-		{ children: "사용 횟수 제한", value: "count" },
-	];
-	const [type, setType] =
-		useState<(typeof GenerateType)[number]["value"]>(
-			"time"
-		);
 	const modalID = "create_invitation";
 	return (
 		<>
 			<Modal modal_id={modalID} outsideClose>
 				<h3>초대 코드 생성</h3>
-				<TabPanel
-					tabs={GenerateType}
-					selectValue={type}
-					setSelectValue={setType}
-				/>
 				<div>
-					ㅋㅋ
+					개발 중인 내용
 					<FilledButton aria-label="copy">
 						<MdOutlineContentCopy />
 					</FilledButton>
